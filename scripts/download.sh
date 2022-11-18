60753029b9cd262bd91e93d6050db7b94593dab5
Noelia Rodriguez <noeliardp@gmail.com>
2022-11-18 23:29:36 +0100

    Add filtering sequences control structure
    Add comments ans wget control structure to download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,32 +1,33 @@
-# This script should download the file specified in the first argument ($1),
-# place it in the directory specified in the second argument ($2),
-# and *optionally*:
-# - uncompress the downloaded file with gunzip if the third
-#   argument ($3) contains the word "yes"
-# - filter the sequences based on a word contained in their header lines:
-#   sequences containing the specified word in their header should be **excluded**
-#
-# Example of the desired filtering:
-#
-#   > this is my sequence
-#   CACTATGGGAGGACATTATAC
-#   > this is my second sequence
-#   CACTATGGGAGGGAGAGGAGA
-#   > this is another sequence
-#   CCAGGATTTACAGACTTTAAA
-#
-#   If $4 == "another" only the **first two sequence** should be output
-
 #Variables definition
 wd=~/decont
 sample_url=$1
 sample_dir=$2
 uncompress=$3
+filter=$4
 
 echo "Downloading $(basename $sample_url .fastq.gz)..."
 wget -c -P $sample_dir $sample_url
+if [ "$?" -ne 0 ] # Checks if previous exit code is not equal to 0
+then
+    echo "Error in downloading file. Usage: bash $0"
+    exit 1 
+fi
+echo "Done"
+echo
+
 if [ "$uncompress" == "yes" ]
 then
 	echo "Uncompressing database..."
-	gunzip $sample_dir/$(basename $sample_url)
+	gunzip -k $sample_dir/$(basename $sample_url)
+	echo "Filtering database..."
+	cat $sample_dir/$(basename $sample_url .gz) | awk '{FS=">"; if (!/small nuclear/) print}' > tmp && mv tmp $wd/$sample_dir/$(basename $sample_url .gz)
+	echo "Done"
+fi
+echo
+
+if [ "$filter" == "small nuclear" ]
+then
+	echo "Filtering database..."
+	cat $wd/$out_dir/$(basename $sample_url .gz) | awk '{FS=">"; if (!/small nuclear/) print}' > tmp && mv tmp $wd/$out_dir/$(basename $sample_url .gz)
+	echo "Done"
 fi
