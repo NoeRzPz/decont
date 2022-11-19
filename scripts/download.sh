c64cf8648a3663e9d2fa23a65aaed241b9c85c8c
Noelia Rodriguez <noeliardp@gmail.com>
2022-11-19 15:33:57 +0100

    Add seqkit command

--- scripts/download.sh
+++ scripts/download.sh
@@ -3,6 +3,7 @@ file_url=$1
 out_dir=$2
 uncompress=$3
 filter=$4
+gunzipfile=$(basename $file_url .gz)
 
 echo "Downloading $(basename $file_url) file..."
 wget -c -P $out_dir $file_url
@@ -24,6 +25,7 @@ echo
 if [ "$filter" == "small nuclear" ]
 then
     echo "Filtering $(basename $file_url .fasta.gz) database..."
-    cat $out_dir/$(basename $file_url .gz) | awk '{FS=">"; if (!/small nuclear/) print}' > tmp && mv tmp $out_dir/$(basename $file_url .gz)
+    seqkit grep -v -r -p "small nuclear" -n $out_dir/$gunzipfile \
+    > tmp && mv tmp $out_dir/$gunzipfile
     echo "Done"
 fi
