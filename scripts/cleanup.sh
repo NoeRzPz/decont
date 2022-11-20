7e67b080d4adbb9d81a1b6bde87aba97db15d21f
Noelia Rodriguez <noeliardp@gmail.com>
2022-11-20 15:27:37 +0100

    Finish implementing last bonus

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,8 +3,8 @@
 
 if [ "$#" -eq 0 ] # If no arguments are passed, it deletes every file
 then
-    find data ~/decont -maxdepth 1 -type f ! \( -name urls -o -name .gitkeep -o -name .gitignore -o -name README.md \) -print
-    find log out res -mindepth 1 ! -name .gitkeep -print
+    find data ~/decont -maxdepth 1 -type f ! \( -name urls -o -name .gitkeep -o -name .gitignore -o -name README.md \) -delete
+    find log out res -mindepth 1 ! -name .gitkeep -delete
     echo "Everything has been deleted"
 else
     args=$@ # Otherwise, it stores all positional arguments in a variable
@@ -15,18 +15,18 @@ for arg in $args
 do
     case $arg in # Use case statement to make decision
         "data") 
-		find $arg -maxdepth 1 -type f ! \( -name urls -o -name .gitkeep \) -print
+		find $arg -maxdepth 1 -type f ! \( -name urls -o -name .gitkeep \) -delete
 		echo "All $arg files deleted"
 		;;
-	"resources") find res -mindepth 1 ! -name .gitkeep -print
+	"resources") find res -mindepth 1 ! -name .gitkeep -delete
 		echo "All resources deleted"
 		;;
-	"output") find out -mindepth 1 ! -name .gitkeep -print
+	"output") find out -mindepth 1 ! -name .gitkeep -delete
 		echo "All outputs deleted"
 		;;
 	"logs") 
-		find log -mindepth 1 ! -name .gitkeep -print
-		find ~/decont -maxdepth 1 -type f ! \( -name .gitignore -o -name README.md \) -print
+		find log -mindepth 1 ! -name .gitkeep -delete
+		find ~/decont -maxdepth 1 -type f ! \( -name .gitignore -o -name README.md \) -delete
 		echo "All logs deleted"
 		;;
 	*) echo "$arg directory doesn't exist in this repository";;
