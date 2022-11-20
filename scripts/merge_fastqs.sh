597c55569b061e1d382d5a02c52a42510c86e5fa
Noelia Rodriguez <noeliardp@gmail.com>
2022-11-20 09:18:48 +0100

    Implement easier solution

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -5,8 +5,3 @@ sampleid=$3
 
 mkdir -p $outdir
 cat $indir/${sampleid}*.fastq.gz > $outdir/${sampleid}.fastq.gz
-if [ "$?" -ne 0 ] # Control structure for previous exit code
-then
-    echo "Error in merging files."
-    exit 1
-fi
--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,33 +1,23 @@
 echo "-------- Starting pipeline at $(date +'%d %h %y, %r')... --------"
 
+#Stop execution when having a non-zero status and trap errors giving line number
+set -e
+trap 'echo Error at about $LINENO' ERR
+
 #Download all the files specified in data/filenames
 for url in $(cat data/urls)
 do
     bash scripts/download.sh $url data
-    if [ "$?" -ne 0 ]
-    then
-        exit 1
-    fi
 done
 
 # Download the contaminants fasta file, uncompress it, and
 # filter to remove all small nuclear RNAs
 bash scripts/download.sh https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz res yes "small nuclear"
-if [ "$?" -ne 0 ]
-then
-    echo "Error in processing database."
-    exit 1
-fi
 echo
 
 # Index the contaminants file
 echo "Running STAR index..."
 bash scripts/index.sh res/contaminants.fasta res/contaminants_idx
-if [ "$?" -ne 0 ]
-then
-    echo "Error in indexing database."
-    exit 1
-fi
 echo "Done"
 echo
 
@@ -36,10 +26,6 @@ for sid in $(ls data/*fastq.gz | xargs basename -a | cut -d"-" -f1 | sort -u)
 do
     echo "Merging sample $sid files..."
     bash scripts/merge_fastqs.sh data out/merged $sid
-    if [ "$?" -ne 0 ]
-    then
-        exit 1
-    fi
     echo "Done"
     echo
 done
@@ -55,18 +41,9 @@ do
         -m 18 \
         -a TGGAATTCTCGGGTGCCAAGG \
         --discard-untrimmed \
-        -o out/trimmed/${sid}.trimmed.fastq.gz out/merged/${#sid}.fastq.gz \
+        -o out/trimmed/${sid}.trimmed.fastq.gz out/merged/${sid}.fastq.gz \
         > log/cutadapt/${sid}.log
-    if [ "$?" -ne 0 ]
-    then
-        fail=1
-    fi
 done
-if [ -n "$fail" ]
-then
-    echo "Error in running cutadapt"
-    exit 1
-fi
 echo "Done"
 echo
 
@@ -84,16 +61,7 @@ do
         --readFilesIn out/trimmed/${sid}.trimmed.fastq.gz \
         --readFilesCommand gunzip -c  \
         --outFileNamePrefix out/star/${sid}/
-    if [ "$?" -ne 0 ]
-    then
-        mistake=1
-    fi
 done 
-if [ -n "$mistake" ]
-then
-    echo "Error in running STAR"
-    exit 1
-fi
 echo "Done"
 echo
 
@@ -106,11 +74,6 @@ do
     cat log/cutadapt/${sid}.log | egrep "Reads with |Total basepairs" >> Log.out
     cat out/star/${sid}/Log.final.out | \
     egrep "reads %|% of reads mapped to (multiple|too)" >> Log.out
-    if [ "$?" -ne 0 ]
-    then
-        echo "Error in creating Log.out"
-	exit 1
-    fi
     echo >> Log.out
 done
 echo "Done"
