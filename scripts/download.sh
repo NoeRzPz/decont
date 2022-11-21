64254419c5a2db2b475cf611b269895f9f6fd711
Noelia Rodriguez <noeliardp@gmail.com>
2022-11-21 05:13:52 +0100

    Move md5 check code to download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -14,16 +14,39 @@ then
         exit 0
     fi
     wget -P $out_dir -i $file_url
+    echo
+
+    echo "Checking md5sum..."
+    for url in $(cat $file_url)
+    do
+        md5sum -c <(echo $(curl ${url}.md5 | grep s_sRNA. | cut -d" " -f1) $out_dir/$(basename $url))
+    done
 else
     echo "Downloading $(basename $file_url) file..."
     if [ -e $out_dir/$(basename $file_url) ] # Check output existence
     then
         echo "File $(basename $file_url) already downloaded"
-        exit 0
+	exit 0
     fi
     wget -P $out_dir $file_url
+    echo
+
+    echo "Checking $(basename $file_url) md5sum..."
+    md5sum -c <(echo $(curl ${file_url}.md5 | grep fasta.gz | cut -d" " -f1) $out_dir/$(basename $file_url))
 fi
 echo
+
+#echo "Downloading $(basename $file_url) file..."
+#if [ -e $out_dir/$(basename $file_url) ] # Check output existence
+#then
+#    echo "File $(basename $file_url) already downloaded"
+#    exit 0
+#fi
+#wget -P $out_dir $file_url
+
+#echo "Checking $(basename $file_url) md5sum..."
+#md5sum -c <(echo $(curl ${file_url}.md5 | grep fasta.gz | cut -d" " -f1) $out_dir/$(basename $file_url))
+#echo
 
 if [ "$uncompress" == "yes" ] # Uncompress file only if third argument is yes
 then
@@ -33,7 +56,7 @@ then
 fi
 echo
 
-if [ "$filter" == "small nuclear" ]
+if [ "$filter" == "small nuclear" ] # Filter only if fourth argument is small nuclear
 then
     echo "Filtering $(basename $file_url .fasta.gz) database..."
     seqkit grep -v -r -p "small nuclear" -n $out_dir/$(basename $file_url .gz) \
