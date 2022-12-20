3d869a3bfaf9472666ae4164ff6b3adb67f630ae
Noelia Rodriguez <noeliardp@gmail.com>
2022-12-20 14:29:12 +0100

    Replace find command for rm

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,10 +1,10 @@
 # Script that removes created files
-# It takes zero or more of the arguments: "data" "resources" "output" "logs"
+# It takes zero or more of the arguments: data resources output logs
 
 if [ "$#" -eq 0 ] # If no arguments are passed, it deletes every file
 then
-    find data ~/decont -maxdepth 1 -type f ! \( -name urls -o -name .gitkeep -o -name .gitignore -o -name README.md \) -delete
-    find log out res -mindepth 1 ! -name .gitkeep -delete
+    rm Log* data/*.fastq.gz
+    rm -r log/* res/* out/*
     echo "Everything has been deleted"
 else
     args=$@ # Otherwise, it stores all positional arguments in a variable
@@ -14,21 +14,20 @@ fi
 for arg in $args
 do
     case $arg in # Use case statement to make decision
-        "data") 
-		find $arg -maxdepth 1 -type f ! \( -name urls -o -name .gitkeep \) -delete
+        "data") rm $arg/*.fastq.gz
 		echo "All $arg files deleted"
 		;;
-	"resources") find res -mindepth 1 ! -name .gitkeep -delete
+	"resources") rm -r res/*
 		echo "All resources deleted"
 		;;
-	"output") find out -mindepth 1 ! -name .gitkeep -delete
+	"output") rm -r out/*
 		echo "All outputs deleted"
 		;;
 	"logs") 
-		find log -mindepth 1 ! -name .gitkeep -delete
-		find ~/decont -maxdepth 1 -type f ! \( -name .gitignore -o -name README.md \) -delete
+		rm -r log/*
+		rm Log*
 		echo "All logs deleted"
 		;;
-	*) echo "$arg directory doesn't exist in this repository";;
+	*) echo "$arg directory doesn't exist in this repository or you've used a wrong name to address it";;
 	esac
 done
